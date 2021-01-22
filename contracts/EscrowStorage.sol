578ff33506f936f82cd59ad0e896da6029a8aab9
FerdiKurt <36864124+FerdiKurt@users.noreply.github.com>
2021-01-23 01:54:52 +0300

    Update EscrowStorage.sol

--- contracts/EscrowStorage.sol
+++ contracts/EscrowStorage.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.8.0;
 import './EscrowStructs.sol';
 
 contract EscrowStorage is EscrowStructs {
-        // state variables also we can call them storage variables
+    // state variables also we can call them storage variables
     address public lawyer;
     mapping(uint => uint) public required;
     mapping(uint => uint) public fulfilled;
